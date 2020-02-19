a592efd6c6daae855448226fa1f32b6e41ee33e1
Ayoub Rossi <ayoub.rossi@student.kuleuven.be>
2020-02-19 13:57:23 +0100

    Implemented insertion sort

--- src/main/java/ayrossi/sorting/InsertionSort.java
+++ src/main/java/ayrossi/sorting/InsertionSort.java
@@ -3,8 +3,14 @@ package ayrossi.sorting;
 public class InsertionSort extends SortAlgorithm {
 
     @Override
-    void sort(Comparable[] c) {
-        //ToDo: Implement this
+    public void sort(Comparable[] c) {
+        for(int i=1; i < c.length; i++) {
+            int currentIndex = i;
+            while(currentIndex > 0 && c[currentIndex].compareTo(c[currentIndex-1]) < 0){
+                swap(c, currentIndex, currentIndex-1);
+                currentIndex--;
+            }
+        }
     }
 
 }
