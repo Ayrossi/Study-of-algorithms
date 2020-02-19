4c3525fd8670371a88ff0c386999d0800e97eca2
Ayoub Rossi <ayoub.rossi@student.kuleuven.be>
2020-02-19 14:28:30 +0100

    Started to implement mergesort

--- src/main/java/ayrossi/sorting/MergeSort.java
+++ src/main/java/ayrossi/sorting/MergeSort.java
@@ -1,10 +1,27 @@
 package ayrossi.sorting;
 
+import java.util.Arrays;
+
 public class MergeSort extends SortAlgorithm {
 
     @Override
-    void sort(Comparable[] c) {
-        //ToDo: Implement this
+    public void sort(Comparable[] c) {
+        if(c.length <= 1) {
+            return;
+        }
+
+        int middle = c.length/2;
+
+        sort(sliceOfArray(c,0, middle));
+        sort(sliceOfArray(c, middle + 1, c.length));
+
+
+    }
+
+    public static Comparable[] sliceOfArray(Comparable[] arr,
+                                        int startIndex,
+                                        int endIndex) {
+        return Arrays.stream(arr, startIndex, endIndex).toArray(Comparable[]::new);
     }
 
 }
